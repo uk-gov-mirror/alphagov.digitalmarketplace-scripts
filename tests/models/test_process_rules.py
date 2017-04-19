29c8d949b43aeab933a6d57efed71632e195a51a
Paul Craig <paul_craig_16@hotmail.com>
2017-04-19 12:32:02 +0100

    test for the newline stuff

--- tests/models/test_process_rules.py
+++ tests/models/test_process_rules.py
@@ -1,5 +1,9 @@
 import pytest
-from dmscripts.models.process_rules import format_datetime_string_as_date, remove_username_from_email_address
+from dmscripts.models.process_rules import (
+    format_datetime_string_as_date,
+    remove_username_from_email_address,
+    replace_newlines_with_spaces
+)
 
 
 def test_format_datetime_string_as_date():
@@ -27,3 +31,27 @@ def test_remove_username_from_email_address():
     initial_email_address = "user.name@domain.com"
     formatted_email_address = "domain.com"
     assert remove_username_from_email_address(initial_email_address) == formatted_email_address
+
+
+test_data = [
+    ("Che Guevara\n", "Che Guevara"),
+    ("Che Guevara\n\r", "Che Guevara"),
+    ("Che Guevara\n\n\n\n\r\r\r\r", "Che Guevara"),
+    ("Che\n\rGuevara", "Che Guevara"),
+    ("Che. \n\rGuevara. \n\r", "Che.  Guevara."),
+    ("\n\rChe Guevara", "Che Guevara"),
+    ("\n\n\r\rChe \n\r \n\r Guevara\n\r", "Che     Guevara"),
+    (
+        """
+The Call-Off contract will be £28m per annum.
+Each Statement of Work shall detail the allocated spend to that particular project.
+        """,
+        "The Call-Off contract will be £28m per annum. "
+        "Each Statement of Work shall detail the allocated spend to that particular project."
+    )
+]
+
+
+@pytest.mark.parametrize("before,after", test_data)
+def test_replace_newlines_with_spaces(before, after):
+    assert replace_newlines_with_spaces(before) == after
