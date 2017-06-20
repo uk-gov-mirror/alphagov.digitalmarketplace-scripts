ceff3543b5d551af4365e7bf7e74516c2e224286
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-06-20 16:39:07 +0100

    Refactor tests

--- tests/test_send_questions_and_answers_email.py
+++ tests/test_send_questions_and_answers_email.py
@@ -11,137 +11,104 @@ from dmscripts.send_questions_and_answers_email import (
     get_ids_of_suppliers_who_asked_a_clarification_question
 )
 
+ALL_BRIEFS = [
+    # a brief with no questions
+    {"id": 0, "clarificationQuestions": []},
+
+    # a brief with a question outside of the date range
+    {"id": 1, "clarificationQuestions": [{"publishedAt": "2017-03-22T06:00:00.669156Z"}]},
+
+    # a brief with two questions outside of the date range
+    {"id": 2, "clarificationQuestions": [
+        {"publishedAt": "2017-03-21T06:00:00.669156Z"},
+        {"publishedAt": "2017-03-22T06:00:00.669156Z"}
+    ]},
+
+    # a brief with a question inside of the date range
+    {"id": 3, "clarificationQuestions": [{"publishedAt": "2017-03-23T06:00:00.669156Z"}]},
+
+    # a brief with two questions inside of the date range
+    {"id": 4, "clarificationQuestions": [
+        {"publishedAt": "2017-03-22T18:00:00.669156Z"},
+        {"publishedAt": "2017-03-23T06:00:00.669156Z"}
+    ]},
+
+    # a brief with two questions, one of them outside the range and one inside the range
+    {"id": 5, "clarificationQuestions": [
+        {"publishedAt": "2017-03-22T06:00:00.669156Z"},
+        {"publishedAt": "2017-03-23T06:00:00.669156Z"}
+    ]},
+
+    # a brief with questions over the weekend
+    {"id": 6, "clarificationQuestions": [
+        {"publishedAt": "2017-03-17T18:00:00.669156Z"},
+        {"publishedAt": "2017-03-18T06:00:00.669156Z"},
+        {"publishedAt": "2017-03-19T06:00:00.669156Z"},  # Sunday
+        {"publishedAt": "2017-03-20T06:00:00.669156Z"},
+    ]}
+]
+
+FILTERED_BRIEFS = [ALL_BRIEFS[3], ALL_BRIEFS[4], ALL_BRIEFS[5]]
+
 
 def test_get_live_briefs_with_new_questions_and_answers_between_two_dates():
     data_api_client = mock.Mock()
-    brief_iter_values = [
-        # a brief with no questions
-        {"clarificationQuestions": []},
 
-        # a brief with a question outside of the date range
-        {"clarificationQuestions": [{"publishedAt": "2017-03-22T06:00:00.669156Z"}]},
-
-        # a brief with two questions outside of the date range
-        {"clarificationQuestions": [
-            {"publishedAt": "2017-03-21T06:00:00.669156Z"},
-            {"publishedAt": "2017-03-22T06:00:00.669156Z"}
-        ]},
-
-        # a brief with a question inside of the date range
-        {"clarificationQuestions": [{"publishedAt": "2017-03-23T06:00:00.669156Z"}]},
-
-        # a brief with two questions inside of the date range
-        {"clarificationQuestions": [
-            {"publishedAt": "2017-03-22T18:00:00.669156Z"},
-            {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-        ]},
-
-        # a brief with two questions, one of them outside the range and one inside the range
-        {"clarificationQuestions": [
-            {"publishedAt": "2017-03-22T06:00:00.669156Z"},
-            {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-        ]},
-
-        # a brief with questions over the weekend
-        {"clarificationQuestions": [
-            {"publishedAt": "2017-03-17T18:00:00.669156Z"},
-            {"publishedAt": "2017-03-18T06:00:00.669156Z"},
-            {"publishedAt": "2017-03-19T06:00:00.669156Z"},  # Sunday
-            {"publishedAt": "2017-03-20T06:00:00.669156Z"},
-        ]}
-    ]
-
-    data_api_client.find_briefs_iter.return_value = iter(brief_iter_values)
+    data_api_client.find_briefs_iter.return_value = iter(ALL_BRIEFS)
     briefs = get_live_briefs_with_new_questions_and_answers_between_two_dates(
         data_api_client, datetime(2017, 3, 22, hour=8), datetime(2017, 3, 23, hour=8)
     )
     data_api_client.find_briefs_iter.assert_called_once_with(status="live", human=True)
-    assert briefs == [
-        {"clarificationQuestions": [{"publishedAt": "2017-03-23T06:00:00.669156Z"}]},
-        {"clarificationQuestions": [
-            {"publishedAt": "2017-03-22T18:00:00.669156Z"},
-            {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-        ]},
-        {"clarificationQuestions": [
-            {"publishedAt": "2017-03-22T06:00:00.669156Z"},
-            {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-        ]},
-    ]
+    assert briefs == FILTERED_BRIEFS
 
 
-def test_get_ids_of_suppliers_who_started_applying():
+@pytest.mark.parametrize("brief,brief_responses,expected_result", [
+    (FILTERED_BRIEFS[0], {"briefResponses": [{"briefId": FILTERED_BRIEFS[0]["id"], "supplierId": 11111}]}, [11111]),
+    (
+        FILTERED_BRIEFS[1],
+        {
+            "briefResponses": [
+                {"briefId": FILTERED_BRIEFS[1]["id"], "supplierId": 11111},
+                {"briefId": FILTERED_BRIEFS[1]["id"], "supplierId": 11112}]
+        },
+        [11111, 11112]
+    ),
+    (FILTERED_BRIEFS[2], {"briefResponses": []}, []),
+])
+def test_get_ids_of_suppliers_who_started_applying(brief, brief_responses, expected_result):
     data_api_client = mock.Mock()
-    briefs = [
-        {
-            "id": 1,
-            "clarificationQuestions": [{"publishedAt": "2017-03-23T06:00:00.669156Z"}]
-        },
-        {
-            "id": 2,
-            "clarificationQuestions": [
-                {"publishedAt": "2017-03-22T18:00:00.669156Z"},
-                {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-            ]
-        },
-        {
-            "id": 3,
-            "clarificationQuestions": [
-                {"publishedAt": "2017-03-22T06:00:00.669156Z"},
-                {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-            ]
-        },
-    ]
+    data_api_client.find_brief_responses.return_value = brief_responses
 
-    data_api_client.find_brief_responses.side_effect = (
-        {"briefResponses": [{"briefId": 1, "supplierId": 11111}]},
-        {"briefResponses": [{"briefId": 2, "supplierId": 11111}, {"briefId": 2, "supplierId": 11112}]},
-        {"briefResponses": []}
-    )
-
-    assert get_ids_of_suppliers_who_started_applying(data_api_client, briefs) == {1: [11111], 2: [11111, 11112], 3: []}
+    assert get_ids_of_suppliers_who_started_applying(data_api_client, brief) == expected_result
 
 
-def test_get_ids_of_suppliers_who_asked_a_clarification_question():
-    data_api_client = mock.Mock()
-    briefs = [
-        {
-            "id": 1,
-            "clarificationQuestions": [{"publishedAt": "2017-03-23T06:00:00.669156Z"}]
-        },
-        {
-            "id": 2,
-            "clarificationQuestions": [
-                {"publishedAt": "2017-03-22T18:00:00.669156Z"},
-                {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-            ]
-        },
-        {
-            "id": 3,
-            "clarificationQuestions": [
-                {"publishedAt": "2017-03-22T06:00:00.669156Z"},
-                {"publishedAt": "2017-03-23T06:00:00.669156Z"}
-            ]
-        },
-    ]
-
-    # we're going to have a list of audit events for clarification questions for each brief
-
-    data_api_client.find_audit_events.side_effect = (
-        # three questions (but only two unique suppliers)
+@pytest.mark.parametrize("brief,audit_events,expected_result", [
+    (
+        FILTERED_BRIEFS[0],
         {"auditEvents": [
-            {"data": {"briefId": 1, "question": "can you help me?", "supplierId": 11111}},
-            {"data": {"briefId": 1, "question": "please can you help me?", "supplierId": 11111}},
-            {"data": {"briefId": 1, "question": "can you help me?", "supplierId": 11112}}
+            {"data": {"briefId": FILTERED_BRIEFS[0]["id"], "question": "can you help me?", "supplierId": 11111}},
+            {"data": {"briefId": FILTERED_BRIEFS[0]["id"], "question": "please can you help me?", "supplierId": 11111}},
+            {"data": {"briefId": FILTERED_BRIEFS[0]["id"], "question": "can you help me?", "supplierId": 11112}}
         ]},
-        # one question
-        {"auditEvents": [{"data": {"briefId": 2, "question": "can you help me?", "supplierId": 11111}}]},
-        # no questions
-        {"auditEvents": []}
-    )
+        [11111, 11111, 11112]
+    ), (
+        FILTERED_BRIEFS[1],
+        {"auditEvents": [{
+            "data": {
+                "briefId": FILTERED_BRIEFS[1]["id"],
+                "question": "can you help me?",
+                "supplierId": 11111
+            }
+        }]},
+        [11111]
+    ),
+    (FILTERED_BRIEFS[2], {"auditEvents": []}, []),
+])
+def test_get_ids_of_suppliers_who_asked_a_clarification_question(brief, audit_events, expected_result):
+    data_api_client = mock.Mock()
+    data_api_client.find_audit_events.return_value = audit_events
 
-    assert get_ids_of_suppliers_who_asked_a_clarification_question(data_api_client, briefs) == {
-        1: [11111, 11111, 11112], 2: [11111], 3: []
-    }
+    assert get_ids_of_suppliers_who_asked_a_clarification_question(data_api_client, brief) == expected_result
 
 
 @pytest.mark.parametrize("number_of_days,start_date,end_date", [
