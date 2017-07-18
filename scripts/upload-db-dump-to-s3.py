1c71f7a8ed6a0ca060e0d332c4428ff4b508ca8c
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-07-18 16:34:03 +0100

    Add scripts to upload db dumps to s3
    The script takes in a stage and file path. It's intended that this
    script will be used by a Jenkins pipeline job which will have already
    created a zipped and encrypted dump.

---
+++ scripts/upload-db-dump-to-s3.py
@@ -0,0 +1,27 @@
+"""
+Usage: scripts/upload-db-dump-to-s3.py <stage> <file_path>
+"""
+
+from docopt import docopt
+from dmutils.s3 import S3
+
+
+def upload_dump(stage, file_path):
+    bucket = S3('digitalmarketplace-database-backup-{}'.format(stage))
+
+    try:
+        with open(file_path) as dump_file:
+            bucket.save('/', dump_file, acl='private')
+    except (OSError, IOError) as e:
+        print("Error reading file '{}': {}".format(file_path, e.message))
+        raise
+    except S3ResponseError as e:
+        print("Error uploading '{}' to bucket: {}".format(file_path, e.message))
+        raise
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    stage = arguments['<stage>']
+    file_path = arguments['<file_path>']
+
+    upload_dump(stage, file_path)
