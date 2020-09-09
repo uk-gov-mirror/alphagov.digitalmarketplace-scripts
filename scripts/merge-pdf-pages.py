d4467188f1a0ab5c0c82943b01d1e16a78165e2b
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-09-09 15:26:11 +0100

    make executable

--- scripts/merge-pdf-pages.py
+++ scripts/merge-pdf-pages.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 from PyPDF2 import PdfFileMerger
 
 pdf_file_merger = PdfFileMerger()
-file = open("myfile.pdf", "w")
+pdf_file_merger.merge(position=1, fileobj='extremely_important.pdf')
 pdf_file_merger.merge(position=0, fileobj='file.pdf')
 pdf_file_merger.write('out.pdf')
